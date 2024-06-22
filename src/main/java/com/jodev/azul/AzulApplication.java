50c0decc5194cae4821fdbe3c7ac43901f96a4d0
Jorge <jorlopeztohme@gmail.com>
2024-06-22 19:42:23 -0300

    First commit

---
+++ src/main/java/com/jodev/azul/AzulApplication.java
@@ -0,0 +1,13 @@
+package com.jodev.azul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jodev/azul/AzulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodev.azul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
